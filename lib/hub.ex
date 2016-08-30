4e0864128736e93a0b09ed5465760bb3a388cf96
byelipk <byelipk@gmail.com>
2016-08-30 08:17:00 -0400

    Create the hub

--- lib/hub.ex
+++ lib/hub.ex
@@ -1,2 +1,21 @@
+require IEx
+
 defmodule Hub do
+  @username "byelipk"
+
+  "https://api.github.com/users/#{@username}/repos"
+    |> HTTPotion.get(headers: ["User-Agent": "Elixir"])
+    |> Map.get(:body)
+    |> Poison.decode!
+    |> Enum.each(fn repo ->
+      def unquote(String.to_atom(repo["name"]))() do
+        unquote(Macro.escape(repo))
+      end
+    end)
+
+  def go(repo) do
+    url = apply(__MODULE__, repo, [])["html_url"]
+    IO.puts "Launching browser to #{url}"
+    System.cmd("open", [url])
+  end
 end
